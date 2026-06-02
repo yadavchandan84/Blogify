7a6d98a2d1811a8427ab990a691e98f0595f1953
Chandan <128459806+yadavchandan84@users.noreply.github.com>
2026-06-02 19:37:10 +0530

    Fix firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'blog-app-d0ef4.firebaseapp.com',
-  projectId: 'blog-app-d0ef4',
-  storageBucket: 'blog-app-d0ef4.firebasestorage.app',
-  messagingSenderId: '361696075720',
-  appId: '1:361696075720:web:85c317c654d7b9b66e2afd'
+  authDomain: "blogify-4740f.firebaseapp.com",
+  projectId: "blogify-4740f",
+  storageBucket: "blogify-4740f.firebasestorage.app",
+  messagingSenderId: "917861207020",
+  appId: "1:917861207020:web:329942801b01def9970ec9"
 };
 
 // Initialize Firebase
